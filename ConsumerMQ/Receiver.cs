f46c7346e259a2b749830df028f8775c50ed80f0
RaghuDesiraju <raghudesiraju@gmail.com>
2020-12-26 18:20:14 -0600

    first commit

---
+++ ConsumerMQ/Receiver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ConsumerMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory=new ConnectionFactory(){HostName="localhost"};
+            //open connection
+            using(var connection=factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("BasicTest",false,false,false, null);
+                    var consumer = new EventingBasicConsumer(channel);
+                    //Console.WriteLine("Checking data received");
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();                      
+                        var message = System.Text.Encoding.UTF8.GetString(body);
+                        Console.WriteLine("Received message " + message);
+                    };
+                    channel.BasicConsume("BasicTest", true, consumer);
+                }
+                 Console.WriteLine("Press any key to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ProducerMQ/Sender.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace ProducerMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory=new ConnectionFactory(){HostName="localhost"};
+            //open connection
+            using(var connection=factory.CreateConnection())
+            {
+                //create fresh channel, session and model
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("BasicTest",false,false,false, null);
+                    string message = "Getting started with .Net core Rabbit MQ";
+                    var body = System.Text.Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish("", "BasicTest", null, body);
+                    Console.WriteLine("Sent message " + message);                    
+                }
+                Console.WriteLine("Press any key to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
